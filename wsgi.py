789d518c88e9524deecf317c2542b96ecd741536
K Lindholm <krista.lindholm@gmail.com>
2018-02-27 23:09:31 +0200

    Adding a log file with a hostname and a timestamp

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 import socket
+import datetime
 from flask import Flask
 
 application = Flask(__name__)
@@ -6,7 +7,12 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    return "Hello World!! Greetings from "+socket.gethostname()+"\n"
+    with open('/mnt/logfile', 'a+') as log:
+        log.write('Greetings from '+socket.gethostname()+' at ' + str(datetime.datetime.now() + '\n')
+        log.seek(0)
+        data = log.read()
+    
+    return data
 
 
 if __name__ == "__main__":
